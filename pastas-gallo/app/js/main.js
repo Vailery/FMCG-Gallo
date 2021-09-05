5f320eee5563af3034f4c2140d173b7397f43e94
Vailery <leraliabik8@gmail.com>
2021-09-05 23:44:59 +0300

    feat: add header & slider
    add header & slider

--- pastas-gallo/app/js/main.js
+++ pastas-gallo/app/js/main.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function() {
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        easing: 'ease',
+        autoplay: true,
+        pauseOnDotsHover: true,
+        touchThreshold: 3
+    });
+})
