8a62232ae03bc56f545b371a06f2ce51c998f5c5
Vailery <leraliabik8@gmail.com>
2021-09-06 23:21:07 +0300

    feat: add footer & next slider
    add footer & next slider

--- pastas-gallo/app/js/main.js
+++ pastas-gallo/app/js/main.js
@@ -1,10 +1,54 @@
 $(document).ready(function() {
-    $('.slider').slick({
+    $('.top-slider').slick({
         arrows: false,
         dots: true,
         easing: 'ease',
-        autoplay: true,
+        autoplay: false,
         pauseOnDotsHover: true,
         touchThreshold: 3
     });
+
+    $('.product-slider').slick({
+        infinite: false,
+        centerMode: true,
+        slidesToShow: 3,
+        dots: true,
+        easing: 'ease',
+        speed: 500,
+        autoplay: false,
+        pauseOnHover: true,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        touchThreshold: 5,
+        autoplaySpeed: 500, //2000
+        waitForAnimate: false,
+        variableWidth: true,
+        initialSlide: 8
+            //убрать стрелочки на 1023
+    })
+
+    $('.product-slider').on('afterChange', function(event, slick, currentSlide) {
+        if (currentSlide == 19) {
+            $('.product-slider').slick('slickGoTo', 0);
+            currentSlide = 0;
+            // $('.product-slider').slick('setOption', 'initialSlide', 0, true);
+            // $('.product-slider').slick('slickPlay')
+            $('.product-slider').slick('slickNext');
+            console.log('Осуществлён переход к' + currentSlide + ' слайду');
+        }
+        // if (currentSlide == 0) {
+        //     $('.product-slider').slick('setOption', 'autoplay', false, false);
+        //     $('.product-slider').slick('setOption', 'initialSlide', 0, true);
+        //     $('.product-slider').slick('setOption', 'autoplay', true, true);
+
+        // }
+
+        // $('.product-slider').slick('slickNext');
+        // if ($('.product-slider').slick('slickPause')) {
+        //     $('.product-slider').slick('slickPlay')
+        //         // $('.product-slider').slick('setOption', 'autoplay', true, true);
+
+        // }
+    })
+
 })
